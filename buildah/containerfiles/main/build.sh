146a9156a3a30b535d425e856c5060d1680309d9
leongross <leon.gross@rub.de>
2022-11-03 13:44:00 +0100

    update COntainerfile to be reproducible

--- buildah/containerfiles/main/build.sh
+++ buildah/containerfiles/main/build.sh
@@ -2,7 +2,7 @@
 BUILDS=2
 
 for i in $(seq $BUILDS); do
-    go build -o main_"$i" -buildvcs=false -trimpath -ldflags "-s -w -buildid=''"
+    CGO_ENABLED=0 go build -o main_"$i" -buildvcs=false -trimpath -ldflags "-s -w -buildid=''"
 done
 
 sha256sum main_*
