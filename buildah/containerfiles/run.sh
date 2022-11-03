7d1b90a4341781eba059ab8dcf527c94892e1e58
leongross <leon.gross@rub.de>
2022-11-03 10:20:47 +0100

    add repush to registry

--- buildah/containerfiles/run.sh
+++ buildah/containerfiles/run.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env bash
-buildah bud --omit-history --timestamp 0 -t buildah-dockerfile
+
+set -x
+DOCKER_REG="leongross/test-buildah"
+AUTH_FILE=$(realpath "$HOME"/.docker/config.json)
+DOCKER_CREDS=$DOCKER_USERNAME:$DOCKER_PASSWORD
+IMAGE_NAME="buildah-dockerfile"
+IMAGE_TAG="buildah-reprod-0"
+
+# build OCI image
+#buildah build \
+#    --omit-history \
+#    --timestamp 0 \
+#    -t $IMAGE_NAME
+
+buildah build \
+    --timestamp 0 \
+    -t $IMAGE_NAME
+
+# push image to (docker) registry
+buildah push \
+    --authfile "$AUTH_FILE" \
+    --digestfile "$IMAGE_NAME"_"$IMAGE_TAG".sha256 \
+    $IMAGE_NAME \
+    docker://"$DOCKER_REG":"$IMAGE_TAG"
